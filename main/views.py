7c3e49a2c690a2bc2653e2902c23a7d89b8f07f5
HaikalMuzaki <haikalmuzaki28@gmail.com>
2025-09-24 16:18:40 +0700

    tutorial 4 selesai

--- main/views.py
+++ main/views.py
@@ -24,9 +24,9 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm': '240123456',
+        'npm': '2406407360',
         'name': request.user.username,
-        'class': 'PBP A',
+        'class': 'PBP B',
         'news_list': news_list,
         'last_login': request.COOKIES.get('last_login', 'Never')
     }
@@ -131,4 +131,21 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
 # Create your views here.
